7413fcc5a573a11e61c1badc9020c117d80705f3
Dragos Dascalita Haut <ddragosd@gmail.com>
2017-02-06 18:35:07 -0800

    updated slack message

--- kegbot-slack-0.0.1.js
+++ kegbot-slack-0.0.1.js
@@ -76437,9 +76437,177 @@ function extend() {
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});/**
  * The entry point for the action.
  * @param params Input object
+ * "data": {
+                    "drink": {
+                        "duration": 8,
+                        "id": 25,
+                        "images": [
+                            {
+                                "keg_id": 1,
+                                "original_url": "http://52.204.245.247:80/media/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8.jpg",
+                                "session_id": 2,
+                                "thumbnail_url": "http://52.204.245.247:80/media/CACHE/images/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8/b7fddff42dc5c48e9ab6acf30db5f4a4.jpg",
+                                "time": "2017-02-07T02:07:06.706214+00:00",
+                                "url": "http://52.204.245.247:80/media/CACHE/images/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8/9635cd5de7ced77f3bb20097b16ba25f.jpg",
+                                "user_id": "guest"
+                            }
+                        ],
+                        "keg": {
+                            "beverage": {
+                                "beverage_type": "beer",
+                                "color_hex": "#C35900",
+                                "id": 1,
+                                "name": "Racer 5",
+                                "producer": {
+                                    "country": "USA",
+                                    "description": "",
+                                    "id": 1,
+                                    "is_homebrew": false,
+                                    "name": "Bear Republic",
+                                    "origin_city": "",
+                                    "origin_state": "",
+                                    "url": ""
+                                },
+                                "style": "IPA"
+                            },
+                            "end_time": "2017-02-04T02:19:41+00:00",
+                            "full_volume_ml": 18927.1,
+                            "id": 1,
+                            "illustration_thumbnail_url": "http://52.204.245.247:80/static/images/keg/thumb/keg-srm14-3.png",
+                            "illustration_url": "http://52.204.245.247:80/static/images/keg/full/keg-srm14-3.png",
+                            "keg_type": "corny",
+                            "online": true,
+                            "percent_full": 85.56519007618225,
+                            "remaining_volume_ml": 16195.009090909089,
+                            "served_volume_ml": 2732.09090909091,
+                            "size": {
+                                "id": 0,
+                                "name": "corny",
+                                "volume_ml": 18927.1
+                            },
+                            "size_id": 0,
+                            "size_name": "corny",
+                            "size_volume_ml": 18927.1,
+                            "spilled_ml": 0,
+                            "spilled_volume_ml": 0,
+                            "start_time": "2017-02-04T02:19:41+00:00",
+                            "type": {
+                                "abv": 0,
+                                "brewer_id": "1",
+                                "id": "1",
+                                "name": "Racer 5",
+                                "style_id": "0"
+                            },
+                            "type_id": "1",
+                            "volume_ml_remain": 16195.009090909089
+                        },
+                        "keg_id": 1,
+                        "session": {
+                            "end_time": "2017-02-07T05:07:06.701949+00:00",
+                            "id": 2,
+                            "name": "",
+                            "start_time": "2017-02-07T02:02:06+00:00",
+                            "url": "/sessions/2017/2/6/2",
+                            "volume_ml": 1888
+                        },
+                        "session_id": 2,
+                        "tick_time_series": "0:1 304:6 1946:36 2116:5 2297:4 2505:5 2709:5 2906:2 3120:4 3302:3 3492:1 3694:1 3902:1 8284:0",
+                        "ticks": 74,
+                        "time": "2017-02-07T02:07:06.701949+00:00",
+                        "url": "/drinks/25",
+                        "user": {
+                            "display_name": "guest",
+                            "is_active": true,
+                            "url": "/drinkers/guest",
+                            "username": "guest"
+                        },
+                        "user_id": "guest",
+                        "volume_ml": 224
+                    },
+                    "drink_id": 25,
+                    "id": 31,
+                    "keg": {
+                        "beverage": {
+                            "beverage_type": "beer",
+                            "color_hex": "#C35900",
+                            "id": 1,
+                            "name": "Racer 5",
+                            "producer": {
+                                "country": "USA",
+                                "description": "",
+                                "id": 1,
+                                "is_homebrew": false,
+                                "name": "Bear Republic",
+                                "origin_city": "",
+                                "origin_state": "",
+                                "url": ""
+                            },
+                            "style": "IPA"
+                        },
+                        "end_time": "2017-02-04T02:19:41+00:00",
+                        "full_volume_ml": 18927.1,
+                        "id": 1,
+                        "illustration_thumbnail_url": "http://52.204.245.247:80/static/images/keg/thumb/keg-srm14-3.png",
+                        "illustration_url": "http://52.204.245.247:80/static/images/keg/full/keg-srm14-3.png",
+                        "keg_type": "corny",
+                        "online": true,
+                        "percent_full": 85.56519007618225,
+                        "remaining_volume_ml": 16195.009090909089,
+                        "served_volume_ml": 2732.09090909091,
+                        "size": {
+                            "id": 0,
+                            "name": "corny",
+                            "volume_ml": 18927.1
+                        },
+                        "size_id": 0,
+                        "size_name": "corny",
+                        "size_volume_ml": 18927.1,
+                        "spilled_ml": 0,
+                        "spilled_volume_ml": 0,
+                        "start_time": "2017-02-04T02:19:41+00:00",
+                        "type": {
+                            "abv": 0,
+                            "brewer_id": "1",
+                            "id": "1",
+                            "name": "Racer 5",
+                            "style_id": "0"
+                        },
+                        "type_id": "1",
+                        "volume_ml_remain": 16195.009090909089
+                    },
+                    "keg_id": 1,
+                    "kind": "drink_poured",
+                    "session": {
+                        "end_time": "2017-02-07T05:07:06.701949+00:00",
+                        "id": 2,
+                        "is_active": true,
+                        "name": "",
+                        "start_time": "2017-02-07T02:02:06+00:00",
+                        "url": "/sessions/2017/2/6/2",
+                        "volume_ml": 1888
+                    },
+                    "session_id": 2,
+                    "time": "2017-02-07T02:07:06.701949+00:00",
+                    "user": {
+                        "date_joined": "2017-02-04T02:13:24+00:00",
+                        "display_name": "guest",
+                        "email": "",
+                        "is_active": true,
+                        "is_staff": false,
+                        "is_superuser": false,
+                        "last_login": "2017-02-04T02:13:24+00:00",
+                        "url": "/drinkers/guest",
+                        "username": "guest"
+                    },
+                    "user_id": "guest"
+                },
+ "param1": "https://hooks.slack.com/services/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",
+ "type": "event"
+ }
  * @returns {Promise}
  */function main(a){return new Promise(function(b,c){//send message to slack channel
-try{var d=require("@slack/client").IncomingWebhook}catch(g){console.log(g),c({message:"Could not load @slack/client",error:g.toString()})}var e=a.slack_webhook_url,f=new d(e);f.send("Hello there from JS",function(g,h){g?(console.log("Error:",g),c(a)):(console.log("Message sent: ",h),b(a))})})}exports.default=main;
+try{var d=require("@slack/client").IncomingWebhook}catch(g){console.log(g),c({message:"Could not load @slack/client",error:g.toString()})}var e=a.slack_webhook_url,f=new d(e);// data.drink.user.display_name  poured data.drink.volume_ml of data.drink.keg.beverage.name
+f.send(data.drink.user.display_name+" poured "+data.drink.volume_ml+" ml of "+data.drink.keg.beverage.name,function(g,h){g?(console.log("Error:",g),c(a)):(console.log("Message sent: ",h),b(a))})})}exports.default=main;
 
 },{"@slack/client":1}]},{},[]);
 var main = require('main-action').default;
--- src/action/slack-action.js
+++ src/action/slack-action.js
@@ -1,6 +1,173 @@
 /**
  * The entry point for the action.
  * @param params Input object
+ * "data": {
+                    "drink": {
+                        "duration": 8,
+                        "id": 25,
+                        "images": [
+                            {
+                                "keg_id": 1,
+                                "original_url": "http://52.204.245.247:80/media/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8.jpg",
+                                "session_id": 2,
+                                "thumbnail_url": "http://52.204.245.247:80/media/CACHE/images/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8/b7fddff42dc5c48e9ab6acf30db5f4a4.jpg",
+                                "time": "2017-02-07T02:07:06.706214+00:00",
+                                "url": "http://52.204.245.247:80/media/CACHE/images/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8/9635cd5de7ced77f3bb20097b16ba25f.jpg",
+                                "user_id": "guest"
+                            }
+                        ],
+                        "keg": {
+                            "beverage": {
+                                "beverage_type": "beer",
+                                "color_hex": "#C35900",
+                                "id": 1,
+                                "name": "Racer 5",
+                                "producer": {
+                                    "country": "USA",
+                                    "description": "",
+                                    "id": 1,
+                                    "is_homebrew": false,
+                                    "name": "Bear Republic",
+                                    "origin_city": "",
+                                    "origin_state": "",
+                                    "url": ""
+                                },
+                                "style": "IPA"
+                            },
+                            "end_time": "2017-02-04T02:19:41+00:00",
+                            "full_volume_ml": 18927.1,
+                            "id": 1,
+                            "illustration_thumbnail_url": "http://52.204.245.247:80/static/images/keg/thumb/keg-srm14-3.png",
+                            "illustration_url": "http://52.204.245.247:80/static/images/keg/full/keg-srm14-3.png",
+                            "keg_type": "corny",
+                            "online": true,
+                            "percent_full": 85.56519007618225,
+                            "remaining_volume_ml": 16195.009090909089,
+                            "served_volume_ml": 2732.09090909091,
+                            "size": {
+                                "id": 0,
+                                "name": "corny",
+                                "volume_ml": 18927.1
+                            },
+                            "size_id": 0,
+                            "size_name": "corny",
+                            "size_volume_ml": 18927.1,
+                            "spilled_ml": 0,
+                            "spilled_volume_ml": 0,
+                            "start_time": "2017-02-04T02:19:41+00:00",
+                            "type": {
+                                "abv": 0,
+                                "brewer_id": "1",
+                                "id": "1",
+                                "name": "Racer 5",
+                                "style_id": "0"
+                            },
+                            "type_id": "1",
+                            "volume_ml_remain": 16195.009090909089
+                        },
+                        "keg_id": 1,
+                        "session": {
+                            "end_time": "2017-02-07T05:07:06.701949+00:00",
+                            "id": 2,
+                            "name": "",
+                            "start_time": "2017-02-07T02:02:06+00:00",
+                            "url": "/sessions/2017/2/6/2",
+                            "volume_ml": 1888
+                        },
+                        "session_id": 2,
+                        "tick_time_series": "0:1 304:6 1946:36 2116:5 2297:4 2505:5 2709:5 2906:2 3120:4 3302:3 3492:1 3694:1 3902:1 8284:0",
+                        "ticks": 74,
+                        "time": "2017-02-07T02:07:06.701949+00:00",
+                        "url": "/drinks/25",
+                        "user": {
+                            "display_name": "guest",
+                            "is_active": true,
+                            "url": "/drinkers/guest",
+                            "username": "guest"
+                        },
+                        "user_id": "guest",
+                        "volume_ml": 224
+                    },
+                    "drink_id": 25,
+                    "id": 31,
+                    "keg": {
+                        "beverage": {
+                            "beverage_type": "beer",
+                            "color_hex": "#C35900",
+                            "id": 1,
+                            "name": "Racer 5",
+                            "producer": {
+                                "country": "USA",
+                                "description": "",
+                                "id": 1,
+                                "is_homebrew": false,
+                                "name": "Bear Republic",
+                                "origin_city": "",
+                                "origin_state": "",
+                                "url": ""
+                            },
+                            "style": "IPA"
+                        },
+                        "end_time": "2017-02-04T02:19:41+00:00",
+                        "full_volume_ml": 18927.1,
+                        "id": 1,
+                        "illustration_thumbnail_url": "http://52.204.245.247:80/static/images/keg/thumb/keg-srm14-3.png",
+                        "illustration_url": "http://52.204.245.247:80/static/images/keg/full/keg-srm14-3.png",
+                        "keg_type": "corny",
+                        "online": true,
+                        "percent_full": 85.56519007618225,
+                        "remaining_volume_ml": 16195.009090909089,
+                        "served_volume_ml": 2732.09090909091,
+                        "size": {
+                            "id": 0,
+                            "name": "corny",
+                            "volume_ml": 18927.1
+                        },
+                        "size_id": 0,
+                        "size_name": "corny",
+                        "size_volume_ml": 18927.1,
+                        "spilled_ml": 0,
+                        "spilled_volume_ml": 0,
+                        "start_time": "2017-02-04T02:19:41+00:00",
+                        "type": {
+                            "abv": 0,
+                            "brewer_id": "1",
+                            "id": "1",
+                            "name": "Racer 5",
+                            "style_id": "0"
+                        },
+                        "type_id": "1",
+                        "volume_ml_remain": 16195.009090909089
+                    },
+                    "keg_id": 1,
+                    "kind": "drink_poured",
+                    "session": {
+                        "end_time": "2017-02-07T05:07:06.701949+00:00",
+                        "id": 2,
+                        "is_active": true,
+                        "name": "",
+                        "start_time": "2017-02-07T02:02:06+00:00",
+                        "url": "/sessions/2017/2/6/2",
+                        "volume_ml": 1888
+                    },
+                    "session_id": 2,
+                    "time": "2017-02-07T02:07:06.701949+00:00",
+                    "user": {
+                        "date_joined": "2017-02-04T02:13:24+00:00",
+                        "display_name": "guest",
+                        "email": "",
+                        "is_active": true,
+                        "is_staff": false,
+                        "is_superuser": false,
+                        "last_login": "2017-02-04T02:13:24+00:00",
+                        "url": "/drinkers/guest",
+                        "username": "guest"
+                    },
+                    "user_id": "guest"
+                },
+ "param1": "https://hooks.slack.com/services/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",
+ "type": "event"
+ }
  * @returns {Promise}
  */
 function main(params) {
@@ -23,7 +190,11 @@ function main(params) {
 
             var webhook = new IncomingWebhook(url);
 
-            webhook.send('Hello there from JS',
+            // data.drink.user.display_name  poured data.drink.volume_ml of data.drink.keg.beverage.name
+
+
+
+            webhook.send(data.drink.user.display_name + " poured " + data.drink.volume_ml + " ml of " + data.drink.keg.beverage.name,
                 function (err, res) {
                     if (err) {
                         console.log('Error:', err);
